32f64f6f29af4e083a4fe3d298ddc06c643c998b
snehal patel <snehal.patel@e-sky.ca>
2022-02-20 14:37:27 -0500

    #5 icons & some styles

--- script.js
+++ script.js
@@ -4,51 +4,44 @@ const ul = document.querySelector('#ul');
 
 const createTodo = (event) => {
   event.preventDefault();
+  if (!todoInput.value) {
+    alert('Please enter Task!');
+    return;
+  }
 
-  //   const todoContainer = document.querySelector('#todo-container');
   const todoContainer = document.createElement('div');
-  todoContainer.classList.add('flex', 'justify-between', 'items-center', 'p-2');
+  todoContainer.classList.add(
+    'flex',
+    'justify-between',
+    'items-center',
+    'p-3',
+    'bg-teal-600',
+    'text-white'
+  );
 
-  // create li and append it to the todo container
   const li = document.createElement('li');
   li.classList.add('flex-1');
   li.textContent = todoInput.value;
   todoContainer.append(li);
 
-  // create complete button and append to the container
   const completeButton = document.createElement('button');
-  completeButton.classList.add(
-    'border-2',
-    'p-2',
-    'mr-2',
-    'border-black',
-    'rounded-full'
-  );
-  completeButton.id = 'complete';
-  completeButton.textContent = 'complete';
+  completeButton.innerHTML = '<i class="fa-solid fa-circle-check"></i>';
+  completeButton.classList.add('mr-5');
+  completeButton.id = 'check';
   todoContainer.append(completeButton);
 
-  // create delete button and append to the container
   const deleteButton = document.createElement('button');
-  deleteButton.classList.add(
-    'border-2',
-    'p-2',
-
-    'border-black',
-    'rounded-full'
-  );
+  deleteButton.innerHTML = '<i class="fa-solid fa-trash"></i>';
   deleteButton.id = 'delete';
-  deleteButton.textContent = 'delete';
   todoContainer.append(deleteButton);
 
-  // append todo container to the ul
   ul.append(todoContainer);
   todoInput.value = '';
 };
 
 const deleteTodo = (event) => {
-  const item = event.target;
-  if (item.id === 'complete') {
+  const item = event.target.parentElement;
+  if (item.id === 'check') {
     item.previousElementSibling.classList.add('line-through');
     item.parentElement.classList.add('bg-red-400');
     item.disabled = true;
